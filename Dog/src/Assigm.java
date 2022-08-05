2d7ba89cd0f7e7cb374d6107594ea0ec35f914cc
Leon Wilson <leondrewwilson@gmail.com>
2022-08-05 14:19:55 +0930

    Added loop that adds random dogs

--- Dog/src/Assigm.java
+++ Dog/src/Assigm.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Assigm {
@@ -19,7 +20,28 @@ public class Assigm {
 
 
         // create a for loop that creates 5 dog with random names and breed
+        Random random = new Random();
+        int m, age;
+        ArrayList<Dogs> dogList = new ArrayList<>();
+        String[] breeds = {"Staffy", "Terrier", "Bulldog", "Labrador"};
 
+        for (int y = 0; y < 5; y++) {
+            m = 0;
+            StringBuilder dogName = new StringBuilder();
+            for (int i = 0; i < 8; i++) {
+                dogName.append((char)('a' + random.nextInt(26)));
+            }
+            m = 1 + random.nextInt(3);
+            StringBuilder dogBreed = new StringBuilder();
+            dogBreed.append(breeds[m]);
+            age = random.nextInt(20);
+            Dogs newDog = new Dogs(dogName.toString(), dogBreed.toString(), age);
+            dogList.add(newDog);
+        }
+
+        for (Dogs dog : dogList) {
+            System.out.println(dog.dogName + " " + dog.dogBreed + " " + dog.dogAge);
+        }
 
     }
 
