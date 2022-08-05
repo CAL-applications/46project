c6b00a8825153c63bc0f58c85b39eaec8205cfc0
Palombi <93624789+Palombi@users.noreply.github.com>
2022-08-05 13:21:03 +0930

    Add files via upload

---
+++ Dog/src/Dogs.java
@@ -0,0 +1,27 @@
+import java.sql.SQLData;
+
+public class Dogs
+{
+
+    public String dogName;
+
+    public String dogBreed;
+
+    public int dogAge;
+
+
+    DBuilt(String name, String breed, int age)
+    {
+
+    dogName= name;
+    dogBreed= breed;
+    dogAge= age;
+
+
+    }
+
+
+
+
+
+}
